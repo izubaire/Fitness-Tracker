e2641ed86e152ede27a6aa5ce1e1a74f861e7a0e
izubaire <m.zobaire@gmail.com>
2021-01-07 15:22:17 +0500

    add routing, configurating MUI and create SignIn page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './config/theme.config';
+
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
-    <div>App</div>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/">
+            <SignIn />
+          </Route>
+          <Route path="/sign-up">
+            Signup
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </Router>
   );
 }
 
